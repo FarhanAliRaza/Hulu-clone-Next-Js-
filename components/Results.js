0e41db80a17be0a8ea1b34b2218f2cfec828381f
Farhan Ali Raza <farhanalirazaazeemi@gmail.com>
2021-09-15 00:03:13 +0500

    last

--- components/Results.js
+++ components/Results.js
@@ -2,9 +2,6 @@ import Thumbnail from "./Thumbnail"
 import FlipMove from "react-flip-move";
 
 function Results({results}) {
-    console.log(results);
-    console.log(typeof results)
-    console.log(results[0])
   
     return (
         <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
